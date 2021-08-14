5311991353603fbd946b86c99ff49e51594e40ba
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2021-08-14 14:39:13 +0300

    fixing mobile layout

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module KanZaman
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
       end if File.exists?(env_file)
-    config.load_defaults 6.0
     end
+    config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
